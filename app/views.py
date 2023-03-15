7cfd26e238cf83b3b4fd57fe8ef7b8684b3ef3d2
vinothg3 <govivinoth2001@gmail.com>
2023-03-15 22:08:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def temp1(request):
+    return render(request,'temp1.html')
+def temp2(request):
+    return render(request,'temp2.html')
